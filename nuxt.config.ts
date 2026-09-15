33185cbff2724597be7dd26a0f675c61a3fccccb
or-abdillh <hans.abdillh05@gmail.com>
2026-09-15 21:32:36 +0800

    Init Nuxt 4 Project - Nuxt UI Starter

---
+++ nuxt.config.ts
@@ -0,0 +1,34 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  modules: [
+    '@nuxt/eslint',
+    '@nuxt/ui',
+    '@nuxt/image',
+    '@nuxt/test-utils',
+    '@nuxtjs/better-auth',
+    '@nuxtjs/google-fonts',
+    '@nuxtjs/supabase',
+    '@pinia/nuxt'
+  ],
+
+  devtools: {
+    enabled: true
+  },
+
+  css: ['~/assets/css/main.css'],
+
+  routeRules: {
+    '/': { prerender: true }
+  },
+
+  compatibilityDate: '2026-06-30',
+
+  eslint: {
+    config: {
+      stylistic: {
+        commaDangle: 'never',
+        braceStyle: '1tbs'
+      }
+    }
+  }
+})
